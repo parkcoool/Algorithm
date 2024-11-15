b0d2bd3093218c83e6ea12e71a42648f9867cf9d
parkcool <yickle1345@naver.com>
2024-11-15 16:37:24 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 996 ms, Memory: 105932 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,29 +1,18 @@
 import sys
-
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(N)]
 
-# cumsum[y][x]: grid[0][x] ~ grid[y][x]까지 합
-cumsum = [[0] * N for _ in range(N)]
-for x in range(N):
-  cumsum[0][x] = grid[0][x]
-  for y in range(1, N):
-    cumsum[y][x] = cumsum[y - 1][x] + grid[y][x]
-
-# dp[y][x]: (0, 0)부터 (y, x)까지 합
-dp = [[0] * N for _ in range(N)]
-for y in range(N):
-  for x in range(N):
-    dp[y][x] = cumsum[y][x]
-    if x > 0: dp[y][x] += dp[y][x - 1]
+# D[y][x]: (1, 1) ~ (y, x)까지의 합
+D = [[0] * (N + 1) for _ in range(N + 1)]
+for y in range(1, N + 1):
+    for x in range(1, N + 1):
+        D[y][x] = D[y][x - 1] + grid[y - 1][x - 1]
+for y in range(1, N + 1):
+    for x in range(1, N + 1):
+        D[y][x] += D[y - 1][x]
 
 for _ in range(M):
-  y1, x1, y2, x2 = map(lambda x: int(x) - 1, input().split())
-  result = dp[y2][x2]
-  if y1 > 0: result -= dp[y1 - 1][x2]
-  if x1 > 0: result -= dp[y2][x1 - 1]
-  if y1 > 0 and x1 > 0:
-    result += dp[y1 - 1][x1 - 1]
-  print(result)
+    y1, x1, y2, x2 = map(int, input().split())
+    print(D[y2][x2] - D[y1 - 1][x2] - D[y2][x1 - 1] + D[y1 - 1][x1 - 1])
