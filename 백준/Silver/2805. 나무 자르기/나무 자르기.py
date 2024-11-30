547f6cb7e19382aef1456a27999754d6a43fe380
parkcool <yickle1345@naver.com>
2024-11-30 19:39:20 +0900

    [Silver II] Title: 나무 자르기, Time: 852 ms, Memory: 146408 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -3,11 +3,16 @@ from bisect import bisect_left
 N, M = map(int, input().split())
 trees = sorted(map(int, input().split()))
 
+# tree_sum[i]: trees[0] ~ trees[i - 1]의 합
+tree_sum = [0] * (N + 1)
+for i in range(1, N + 1):
+  tree_sum[i] = tree_sum[i - 1] + trees[i - 1]
+
 lo, hi = 0, trees[N - 1]
 while lo <= hi:
   mid = (lo + hi) // 2
   start_index = bisect_left(trees, mid)
-  gain = sum(trees[start_index:]) - mid * (N - start_index)
+  gain = (tree_sum[N] - tree_sum[start_index]) - mid * (N - start_index)
 
   if gain >= M:
     lo = mid + 1
