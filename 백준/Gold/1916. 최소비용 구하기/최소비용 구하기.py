529262b9535fb2f4c5e2b6929c56c5b6330dfdff
parkcool <yickle1345@naver.com>
2024-12-08 13:31:16 +0900

    [Gold V] Title: 최소비용 구하기, Time: 196 ms, Memory: 40172 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,31 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+graph = {n: {} for n in range(N)}
+
+for _ in range(M):
+    start, dest, cost = map(int, input().split())
+    start, dest = start - 1, dest - 1
+    if dest in graph[start]: graph[start][dest] = min(graph[start][dest], cost)
+    else: graph[start][dest] = cost
+
+start, dest = map(lambda x: int(x) - 1, input().split())
+
+costs = [float("inf")] * N
+costs[start] = 0
+q = []
+heapq.heappush(q, (0, start))
+
+while q:
+    cost, node = heapq.heappop(q)
+    if costs[node] < cost: continue
+    for next_node in graph[node]:
+        next_cost = cost + graph[node][next_node]
+        if next_cost >= costs[next_node]: continue
+        costs[next_node] = next_cost
+        heapq.heappush(q, (next_cost, next_node))
+
+print(costs[dest])
