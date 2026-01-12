cd097312c790fa71ba34d17f6806c4a58e174df6
parkcool <yickle1345@naver.com>
2026-01-12 18:01:28 +0900

    [level 2] Title: 전화번호 목록, Time: 95.76 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,7 @@
 def solution(phone_book):
     sorted_phone_book = sorted(phone_book)
     for i in range(len(phone_book) - 1):
-        phone = sorted_phone_book[i]
-        if sorted_phone_book[i + 1][:len(phone)] == phone: return False
+        if sorted_phone_book[i + 1].startswith(sorted_phone_book[i]):
+            return False
     
     return True
