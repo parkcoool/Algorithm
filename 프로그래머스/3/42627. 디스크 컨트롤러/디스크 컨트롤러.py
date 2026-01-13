6b7f168bcf296f01c641232ae434239aa43f8979
parkcool <yickle1345@naver.com>
2026-01-13 13:02:59 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.18 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -9,22 +9,22 @@ def solution(jobs):
     # 현재 진행 중인 작업이 끝나고 진행할 작업 대기열 (소요 시간, 요청 시각)
     h = []
     
-    while h or job_index < len(jobs):
-            
+    while h or job_index < len(jobs):   
         # 현재 진행 중인 작업이 끝나기 이전에 요청되는 작업을 h에 추가
         while job_index < len(jobs) and sorted_jobs[job_index][0] <= current_end:
             heapq.heappush(h, (sorted_jobs[job_index][1], sorted_jobs[job_index][0]))
             job_index += 1
 
-        # 현재 진행 중인 작업 종료
+        # 대기 중인 작업이 있으면
         if h:
+            # 현재 작업 종료 후 바로 시작
             current = heapq.heappop(h)
             current_end = current_end + current[0]
             ans += current_end - current[1]
+        
+        # 없으면
         else:
-            # 현재 진행 중인 작업이 끝나기 이전에 요청되는 작업이 없으면
-            if job_index < len(jobs) and sorted_jobs[job_index][0] > current_end:
-                # 대기
-                current_end = sorted_jobs[job_index][0]
+            # 대기
+            current_end = sorted_jobs[job_index][0]
         
     return ans // len(jobs)
