2a8534feeddbb6a49832ff0ddfe8a0aac1720e18
parkcool <yickle1345@naver.com>
2026-01-13 14:51:01 +0900

    [level 3] Title: 이중우선순위큐, Time: 105.24 ms, Memory: 21.2 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,22 +1,39 @@
 import heapq
 
 def solution(operations):
-    q = []
+    # (value, id)
+    min_h = []
+    max_h = []
     
-    for operation in operations:
-        command, num = operation.split() 
-        num = int(num)
+    # dead[id]: id에 해당하는 값이 삭제됐는지 여부
+    dead = [False] * len(operations)
+    
+    for id, operation in enumerate(operations):
+        command, value = operation.split(" ")
+        value = int(value)
         
         if command == "I":
-            heapq.heappush(q, num)
-        elif command == "D":
-            if num == 1 and q:
-                max_value = max(q)
-                q.remove(max_value)
-            elif num == -1 and q:
-                heapq.heappop(q)
-    
-    if not q: return [0, 0]
-    else: return [max(q), heapq.heappop(q)]
+            heapq.heappush(min_h, (value, id))
+            heapq.heappush(max_h, (-value, id))
         
-    return answer
+        elif command == "D":
+            if value == -1:
+                while min_h and dead[min_h[0][1]]:
+                    heapq.heappop(min_h)
+                if min_h:
+                    min_value, min_id = heapq.heappop(min_h)
+                    dead[min_id] = True
+                    
+            elif value == 1:
+                while max_h and dead[max_h[0][1]]:
+                    heapq.heappop(max_h)
+                if max_h:
+                    max_value, max_id = heapq.heappop(max_h)
+                    dead[max_id] = True
+                    
+    
+    while min_h and dead[min_h[0][1]]: heapq.heappop(min_h)
+    while max_h and dead[max_h[0][1]]: heapq.heappop(max_h)
+    
+    if min_h and max_h: return [-max_h[0][0], min_h[0][0]]
+    return [0, 0]
