46fb17df2cbf529a4a1304ffbc269379dd94561f
parkcool <yickle1345@naver.com>
2026-01-13 15:54:29 +0900

    [level 2] Title: 가장 큰 수, Time: 2261.18 ms, Memory: 23.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,5 +1,18 @@
+import functools
+
+def compare(a, b):
+    t1 = a + b
+    t2 = b + a
+    
+    if int(t1) > int(t2):
+        return 1
+    elif int(t1) < int(t2):
+        return -1
+    else:
+        return 0
+
 def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x * 3, reverse=True)
-    answer = "".join(numbers)
+    n_str = [str(x) for x in numbers]
+    n_str.sort(key=functools.cmp_to_key(compare), reverse=True)
+    answer = "".join(n_str)
     return str(int(answer))
