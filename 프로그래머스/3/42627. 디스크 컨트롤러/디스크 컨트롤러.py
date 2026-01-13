cbb9ed3f378db92ff49ace28372f1f7845fccaf8
parkcool <yickle1345@naver.com>
2026-01-13 12:50:58 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 1.11 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,24 +1,31 @@
 import heapq
 
 def solution(jobs):
-    timeline = sorted(jobs)
+    sorted_jobs = sorted(jobs)
     job_index = 0
     current_end = 0
     ans = 0
+    
+    # 현재 진행 중인 작업이 끝나고 진행할 작업 대기열 (소요 시간, 요청 시각)
     h = []
     
-    while h or job_index < len(timeline):
-        if job_index < len(timeline) and timeline[job_index][0] > current_end and not h:
-            current_end = timeline[job_index][0]
+    while h or job_index < len(jobs):
             
-        while job_index < len(timeline) and timeline[job_index][0] <= current_end:
-            heapq.heappush(h, (timeline[job_index][1], timeline[job_index][0]))
+        # 현재 진행 중인 작업이 끝나기 이전에 요청되는 작업을 h에 추가
+        while job_index < len(jobs) and sorted_jobs[job_index][0] <= current_end:
+            heapq.heappush(h, (sorted_jobs[job_index][1], sorted_jobs[job_index][0]))
             job_index += 1
-        
+
+        # 현재 진행 중인 작업 종료
         if h:
             current = heapq.heappop(h)
             current_end = max(current_end, current[1]) + current[0]
-            print(f"{max(current_end, current[1])}ms에 {current} 작업 수행 시작")
             ans += current_end - current[1]
-    
+        else:
+            
+            # 현재 진행 중인 작업이 끝나기 이전에 요청되는 작업이 없으면
+            if job_index < len(jobs) and sorted_jobs[job_index][0] > current_end:
+                # 대기
+                current_end = sorted_jobs[job_index][0]
+        
     return ans // len(jobs)
