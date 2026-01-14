50f5367c605cb4c19b608c821a61818ea0c6ba64
parkcool <yickle1345@naver.com>
2026-01-14 16:23:05 +0900

    [level 2] Title: 더 맵게, Time: 1654.40 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -4,13 +4,9 @@ def solution(scoville, K):
     h = scoville[:]
     heapq.heapify(h)
     ans = 0
-    while len(h) >= 2:
-        s1, s2 = heapq.heappop(h), heapq.heappop(h)
-        if s1 >= K: return ans
+    while len(h) >= 2 and h[0] < K:
+        a, b = heapq.heappop(h), heapq.heappop(h)
+        heapq.heappush(h, a + b * 2)
         ans += 1
-        heapq.heappush(h, s1 + s2 * 2)
-    
-    if h and h[0] >= K: return ans
-    
-    return -1
-        
+    if h and h[0] < K: return -1
+    return ans
