fd63d5213cd70804e6020528ff3316dc03e6f301
parkcool <yickle1345@naver.com>
2026-01-14 13:18:47 +0900

    [level 2] Title: 주식가격, Time: 34.11 ms, Memory: 19.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,15 +1,9 @@
 def solution(prices):
-    indexes = []
-    ans = [-1] * len(prices)
-    
-    for index in range(len(prices)):
-        while indexes and prices[indexes[-1]] > prices[index]:
-            past = indexes.pop()
-            ans[past] = index - past
-        
-        indexes.append(index)
-    
-    for index in indexes:
-        ans[index] = len(prices) - index - 1
-    
+    ans = [x for x in range(len(prices) - 1, -1, -1)]
+    stack = []
+    for index, price in enumerate(prices):
+        while stack and stack[-1][1] > price:
+            past_index, past_price = stack.pop()
+            ans[past_index] = index - past_index          
+        stack.append((index, price))
     return ans
