9d2a916a3ab68a34d140f263d36cf5a4b2c33427
parkcool <yickle1345@naver.com>
2026-01-25 15:14:06 +0900

    [level 3] Title: 등굣길, Time: 3.49 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,12 +1,12 @@
 def solution(m, n, puddles):
-    dp = [[0] * m for _ in range(n)]
-    dp[0][0] = 1
-    for y in range(n):
-        for x in range(m):
-            if y == 0 and x == 0: continue
-            if [x + 1, y + 1] in puddles: continue
-            count1 = dp[y - 1][x] if y > 0 else 0
-            count2 = dp[y][x - 1] if x > 0 else 0
-            dp[y][x] = count1 + count2
-    return dp[n - 1][m - 1] % 1_000_000_007
+    # dp[y][x]: (1, 1)에서 (y, x)까지 오는 경우의 수
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    dp[1][1] = 1
     
+    for y in range(1, n + 1):
+        for x in range(1, m + 1):
+            if [x, y] in puddles: continue
+            if y == 1 and x == 1: continue
+            dp[y][x] = dp[y - 1][x] + dp[y][x - 1]
+    
+    return dp[n][m] % 1_000_000_007
