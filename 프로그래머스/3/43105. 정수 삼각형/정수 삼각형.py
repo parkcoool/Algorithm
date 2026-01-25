19628877d2c11d044e98cd103ade4a5678589997
parkcool <yickle1345@naver.com>
2026-01-25 14:32:29 +0900

    [level 3] Title: 정수 삼각형, Time: 90.64 ms, Memory: 18.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,8 +1,10 @@
 def solution(triangle):
-    # dp[y][x]: (y, x)가 꼭대기인 삼각형에서의 답
-    dp = [[-1] * len(x) for x in triangle]
-    dp[-1] = triangle[-1][:]
-    for y in range(len(triangle) - 2, -1, -1):
+    dp = [[triangle[0][0]]]
+    for y in range(1, len(triangle)):
+        dp.append([])
         for x in range(y + 1):
-            dp[y][x] = triangle[y][x] + max(dp[y + 1][x], dp[y + 1][x + 1])
-    return dp[0][0]
+            result = 0
+            if x > 0: result = max(result, dp[y - 1][x - 1] + triangle[y][x])
+            if x < y: result = max(result, dp[y - 1][x] + triangle[y][x])
+            dp[-1].append(result)
+    return max(dp[-1])
