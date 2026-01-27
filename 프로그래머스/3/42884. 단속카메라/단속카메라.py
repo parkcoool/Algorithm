176c91bb9702c02c5084001ce14933382b182627
parkcool <yickle1345@naver.com>
2026-01-27 12:48:37 +0900

    [level 3] Title: 단속카메라, Time: 2.19 ms, Memory: 9.82 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -2,13 +2,10 @@ def solution(routes):
     routes.sort(key=lambda x: (x[1], x[0]))
     
     ans = 0
-    visited = set()
-    for i1, (s1, e1) in enumerate(routes):
-        if i1 in visited: continue
-        visited.add(i1)
-        ans += 1
-        for i2, (s2, e2) in enumerate(routes):
-            if i2 in visited: continue
-            if s2 <= e1: visited.add(i2)
+    last_camera = -30001 
+    for start, end in routes:
+        if start > last_camera:
+            ans += 1
+            last_camera = end
     
     return ans
