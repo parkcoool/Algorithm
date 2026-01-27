c6a38558679e705141425c8cdeda2a5089c6fbd1
parkcool <yickle1345@naver.com>
2026-01-27 16:44:37 +0900

    [level 3] Title: 단어 변환, Time: 0.82 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,23 +1,19 @@
-from collections import defaultdict, deque
+from collections import deque
 
 def solution(begin, target, words):
-    graph = defaultdict(set)
-    for word1 in words + [begin]:
-        for word2 in words:
-            if word1 == word2: continue
-            diff = 0
-            for i in range(len(word1)):
-                if word1[i] != word2[i]: diff += 1
-            if diff == 1:
-                graph[word1].add(word2)
-    
     q = deque([(begin, 0)])
     visited = set()
     while q:
-        current, dist = q.popleft()
-        if current == target: return dist
-        for word in graph[current]:
-            if word in visited: continue
-            visited.add(word)
-            q.append((word, dist + 1))
+        word, dist = q.popleft()
+        if word == target:
+            return dist
+        
+        for next_word in words:
+            if next_word in visited: continue
+            diff_count = 0
+            for i in range(len(word)):
+                if word[i] != next_word[i]: diff_count += 1
+            if diff_count != 1: continue
+            visited.add(next_word)
+            q.append((next_word, dist + 1))
     return 0
