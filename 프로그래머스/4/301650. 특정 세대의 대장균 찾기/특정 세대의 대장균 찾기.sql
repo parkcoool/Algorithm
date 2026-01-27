bd98be46cf46090644ee5337a873b311d3ea3b46
parkcool <yickle1345@naver.com>
2026-01-27 17:28:52 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,7 +1,17 @@
-SELECT ID FROM ECOLI_DATA WHERE PARENT_ID IN (
-    SELECT ID FROM ECOLI_DATA WHERE PARENT_ID IN (
-        SELECT ID
-        FROM ECOLI_DATA
-        WHERE PARENT_ID IS NULL
-    )
-) ORDER BY ID ASC;
+with recursive generation as (
+    select
+        ID,
+        1 as GENERATION
+    from ECOLI_DATA
+    where PARENT_ID is NULL
+    
+    union all
+    
+    select
+        c.ID,
+        GENERATION + 1 as GENERATION
+    from generation p join ECOLI_DATA c on p.ID = c.PARENT_ID
+    where GENERATION < 3
+)
+
+select ID from generation where GENERATION = 3 order by ID asc;
