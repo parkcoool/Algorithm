b2b79d2be592bb57941de6a1087694e448b82743
parkcool <yickle1345@naver.com>
2026-01-27 11:50:23 +0900

    [level 2] Title: 구명보트, Time: 9.06 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -3,13 +3,10 @@ from collections import deque
 def solution(people, limit):
     q = deque(sorted(people))
     ans = 0
-
     while q:
-        ans += 1
         right = q.pop()
-        if not q: break
-        left = q[0]
-        if left + right <= limit:
+        ans += 1
+        if q and q[0] + right <= limit:
             q.popleft()
-            
+    
     return ans
