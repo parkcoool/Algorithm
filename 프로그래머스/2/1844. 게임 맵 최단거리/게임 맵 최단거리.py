72e5b805639ce0446fce51755164fc68389d2b7f
parkcool <yickle1345@naver.com>
2026-01-27 14:56:03 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.15 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -4,19 +4,17 @@ def solution(maps):
     height = len(maps)
     width = len(maps[0])
     
-    q = deque([(1, 0, 0)])
-    visited = set()
+    maps[0][0] = 0
+    q = deque([(0, 0, 1)])
     while q:
-        dist, y, x = q.popleft()
-        
-        if y == height - 1 and x == width - 1:
-            return dist
-        
-        for new_y, new_x in ((y + 1, x), (y, x + 1), (y, x - 1), (y - 1, x)):
-            if not (0 <= new_y < height and 0 <= new_x < width): continue
+        y, x, dist = q.popleft()
+        if y == height - 1 and x == width - 1: return dist
+        for dy, dx in ((-1, 0), (0, -1), (0, 1), (1, 0)):
+            new_y, new_x = y + dy, x + dx;
+            if not (0 <= new_y < height): continue
+            if not (0 <= new_x < width): continue
             if maps[new_y][new_x] == 0: continue
-            if (new_y, new_x) in visited: continue
-            visited.add((new_y, new_x))
-            q.append((dist + 1, new_y, new_x))
-    
+            maps[new_y][new_x] = 0
+            q.append((new_y, new_x, dist + 1))
+            
     return -1
