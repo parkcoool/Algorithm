3e7d092929a97b0ac5fff19bde6f6a641b1e0b10
parkcool <yickle1345@naver.com>
2026-01-27 11:35:06 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
 
 ### 성능 요약
 
-메모리: 9.3 MB, 시간: 0.02 ms
+메모리: 9.18 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 13일 21:20:51
+2026년 01월 27일 11:35:05
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,22 +1,19 @@
+from math import ceil
+
 def solution(name):
-    ans = 0
-    wrongs = list(filter(lambda i: name[i] != "A", range(len(name))))
+    n = len(name)
     
-    for wrong in wrongs:
-        ord1 = ord("A")
-        ord2 = ord(name[wrong])
-        ans += min(abs(ord2 - ord1), ord("Z") + 1 - ord2)
+    ans = n - 1
+    for i in range(n):
+        next_i = i + 1
+        while next_i < n and name[next_i] == "A":
+            next_i += 1
+            
+        dist1 = i * 2 + n - next_i
+        dist2 = i + (n - next_i) * 2
+        ans = min(ans, dist1, dist2)
     
-    if len(wrongs) > 1:
-        shortest_dist = wrongs[-1]
-        for index in range(len(wrongs) - 1):
-            dist1 = wrongs[index]
-            dist2 = len(name) - wrongs[index + 1]
-            dist = dist1 + dist2 + min(dist1, dist2)
-            shortest_dist = min(dist, shortest_dist)
-        ans += shortest_dist
-        
-    elif len(wrongs) == 1:
-        ans += min(wrongs[0], len(name) - wrongs[0])        
-        
+    for c in name:
+        ans += min(ord(c) - ord("A"), ord("Z") - ord(c) + 1)
+    
     return ans
