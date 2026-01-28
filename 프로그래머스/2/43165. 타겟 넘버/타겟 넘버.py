be727bfe023f4201614a1f5d3548f3611272506f
parkcool <yickle1345@naver.com>
2026-01-28 13:21:13 +0900

    [level 2] Title: 타겟 넘버, Time: 387.59 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,10 +1,11 @@
-from itertools import product
-
 def solution(numbers, target):
-    ans = 0
-    for operations in product(["+", "-"], repeat=len(numbers)):
-        num = 0
-        for i in range(len(numbers)):
-            num += numbers[i] * (1 if operations[i] == "+" else -1)
-        if num == target: ans += 1
-    return ans
+    def dfs(index, number):
+        if index == len(numbers):
+            if number == target: return 1
+            else: return 0
+        
+        result = dfs(index + 1, number + numbers[index])
+        result += dfs(index + 1, number - numbers[index])
+        return result
+        
+    return dfs(0, 0)
