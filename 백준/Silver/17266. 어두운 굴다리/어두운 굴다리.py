a2886054b8403c67d6c73d31c581baa7c5090317
parkcool <yickle1345@naver.com>
2026-02-01 12:03:52 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 84 ms, Memory: 42660 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.py
@@ -1,28 +1,15 @@
 import sys
-
 input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
 points = list(map(int, input().split()))
 
-ans = 0
-lo, hi = 0, N
-while lo <= hi:
-  h = (lo + hi) // 2
-  
-  last = 0
-  dark = 0
-  
-  for point in points:
-    dark += max(0, point - h - last)
-    last = point + h
-  dark += max(0, N - last)
+height = points[0]
+height = max(height, N - points[-1])
 
-  if dark > 0:
-    lo = h + 1
-  else:
-    ans = h
-    hi = h - 1
+for i in range(len(points) - 1):
+    gap = points[i + 1] - points[i]
+    height = max(height, (gap + 1) // 2)
 
-print(ans)
+print(height)
