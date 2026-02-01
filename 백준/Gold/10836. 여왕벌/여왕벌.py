ac2f532d7a14c436fdc101e3c6ad3d33834be80c
parkcool <yickle1345@naver.com>
2026-02-01 13:19:45 +0900

    [Gold III] Title: 여왕벌, Time: 4416 ms, Memory: 36216 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+M, N = map(int, input().split())
+grid = [[1] * M for _ in range(M)]
+
+for _ in range(N):
+  growths = []
+  for num, count in enumerate(map(int, input().split())):
+    growths += [num] * count
+
+  for i, growth in enumerate(growths):
+    grid[max(0, M - 1 - i)][max(0, -M + 1 + i)] += growth
+  
+  for y in range(1, M):
+    for x in range(1, M):
+      grid[y][x] += max(growths[M - 1 - y:M + x])
+
+for row in grid:
+  print(*row)
