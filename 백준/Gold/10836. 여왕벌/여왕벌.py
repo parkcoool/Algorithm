17780f652dd7c604819706620716d8990253ca8f
parkcool <yickle1345@naver.com>
2026-02-01 14:02:53 +0900

    [Gold III] Title: 여왕벌, Time: 1112 ms, Memory: 36348 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.py
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -4,13 +4,23 @@ input = sys.stdin.readline
 M, N = map(int, input().split())
 grid = [[1] * M for _ in range(M)]
 
+growth_diffs = [0] * (2 * M - 1)
 for _ in range(N):
-  growths = []
-  for num, count in enumerate(map(int, input().split())):
-    growths += [num] * count
+  zero, one, two = map(int, input().split())
+  if one:
+    growth_diffs[zero] += 1
+  if one and two:
+    growth_diffs[zero + one] += 1
+  elif not one and two:
+    growth_diffs[zero + one] += 2
 
-  for i, growth in enumerate(growths):
-    grid[max(0, M - 1 - i)][max(0, -M + 1 + i)] += growth
+growths = [0] * (2 * M - 1)
+for i, amount in enumerate(growth_diffs):
+  if i == 0: growths[i] = growth_diffs[i]
+  else: growths[i] = growths[i - 1] + growth_diffs[i]
+
+for i, growth in enumerate(growths):
+  grid[max(0, M - 1 - i)][max(0, -M + 1 + i)] += growth
   
 for y in range(1, M):
   for x in range(1, M):
