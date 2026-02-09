e73b2dd3f88255fc7ff96e931b2a1c60b1869387
parkcool <yickle1345@naver.com>
2026-02-09 23:24:51 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2392 ms, Memory: 70704 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -9,23 +9,23 @@ for _ in range(int(input())):
   graph = [0] + list(map(int, input().strip().split()))
   visited = [False] * (N + 1)
 
-  def dfs(node, path):
+  def dfs(node, path, depth=1):
     visited[node] = True
-    path.append(node)
+    path[node] = depth
     
     next_node = graph[node]
     
     if visited[next_node]:
       if next_node in path:
-        return len(path) - path.index(next_node)
+        return depth - path[next_node] + 1
       else:
         return 0
     else:
-      return dfs(next_node, path)
+      return dfs(next_node, path, depth + 1)
 
   ans = N
   for i in range(1, N + 1):
     if visited[i]: continue
-    ans -= dfs(i, [])
+    ans -= dfs(i, {})
 
   print(ans)
