8a1639dbf3d040dc76eda36ddd7a1298dd3d99e7
parkcool <yickle1345@naver.com>
2026-02-15 16:35:33 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 328 ms, Memory: 67392 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,29 +1,35 @@
 import sys
-import heapq
 from collections import defaultdict
+import heapq
 
 input = sys.stdin.readline
 
 V, E = map(int, input().strip().split())
 
-# (비용, 노드)
+# (cost, node)
 graph = defaultdict(list)
 for _ in range(E):
   a, b, c = map(int, input().strip().split())
   graph[a].append((c, b))
   graph[b].append((c, a))
 
-pq = [(0, 1)]
-visited = [0] * (V + 1)
 ans = 0
+visited = set()
 
+# (cost, node)
+pq = [(0, 1)]
 while pq:
   cost, node = heapq.heappop(pq)
-  if visited[node]: continue
+  if node in visited: continue
+  
+  visited.add(node)
   ans += cost
-  visited[node] = 1
-  for next_cost, next_node in graph[node]:
-    if visited[next_node]: continue
-    heapq.heappush(pq, (next_cost, next_node))
 
+  if len(visited) == V:
+    break
+
+  for next_cost, next_node in graph[node]:
+    if next_node in visited: continue
+    heapq.heappush(pq, (next_cost, next_node))
+    
 print(ans)
