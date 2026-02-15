43b10f5fc06d81b9b454d5cc964692ef9945140d
parkcool <yickle1345@naver.com>
2026-02-15 16:15:15 +0900

    [Gold IV] Title: 호텔, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.py
+++ 백준/Gold/1106. 호텔/호텔.py
@@ -5,20 +5,13 @@ input = sys.stdin.readline
 C, N = map(int, input().strip().split())
 cities = [list(map(int, input().strip().split())) for _ in range(N)]
 
-# dp[i]: i명의 고객을 늘리기 위해 투자해야 하는 돈의 최솟값
+# dp[i]: 고객을 i명 늘리기 위한 최소 비용
 dp = [float("inf")] * (C + 1)
 dp[0] = 0
 
-ans = float("inf")
-for current_value in range(C + 1):
-  for j in range(N):
-      cost, value = cities[j]
-      next_value = current_value + value
-      
-      if next_value > C:
-        ans = min(ans, dp[current_value] + cost)
-      else:
-        dp[next_value] = min(dp[next_value], dp[current_value] + cost)
+for i in range(0, C):
+  for cost, reward in cities:
+    j = min(C, i + reward)
+    dp[j] = min(dp[j], dp[i] + cost)
 
-ans = min(ans, dp[C])
-print(ans)
+print(dp[C])
