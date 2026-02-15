cca8bcb603522a2cef66e89b6bf51206505beb0a
parkcool <yickle1345@naver.com>
2026-02-15 15:51:46 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1120 ms, Memory: 49728 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -8,16 +8,20 @@ for _ in range(int(input())):
   graph = [0] + list(map(int, input().strip().split()))
 
   indegrees = [0] * (N + 1)
-  for target in graph[1:]:
+  for target in graph:
     indegrees[target] += 1
 
+  q = deque([])
+  for i in range(1, N + 1):
+    if indegrees[i] == 0:
+      q.append(i)
+
   ans = 0
-  q = deque([i for i in range(1, N + 1) if indegrees[i] == 0])
   while q:
     current = q.popleft()
-    next_node = graph[current]
     ans += 1
-
+    
+    next_node = graph[current]
     indegrees[next_node] -= 1
     if indegrees[next_node] == 0:
       q.append(next_node)
