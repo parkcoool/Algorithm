defd89757456259b70e2aff74ad9103cefbd9984
parkcool <yickle1345@naver.com>
2026-03-03 11:16:27 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 6060 ms, Memory: 54044 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -1,45 +1,41 @@
 import sys
 from collections import deque
 
+KNIGHT = ((-2, 1), (-2, -1), (2, 1), (2, -1), (-1, 2), (-1, -2), (1, -2), (1, 2))
+NORMAL = ((0, 1), (0, -1), (-1, 0), (1, 0))
+
 input = sys.stdin.readline
 
 K = int(input())
-W, H = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(H)]
+W, H = map(int, input().strip().split())
+grid = [list(map(int, input().strip().split())) for _ in range(H)]
 
-q = deque([(0, 0, 0)])
-# dp[y][x][z]: 말의 움직임을 z번 사용했을 때 (0, 0)에서 (y, x)까지의 최소 동작 수
-dp = [[[-1] * (K + 1) for _ in range(W)] for __ in range(H)]
+# dp[y][x][k]: 말의 움직임으로 k번 움직였을 때 (y, x)까지의 최소 비용
+dp = [[[float("inf")] * (K + 1) for _ in range(W)] for _ in range(H)]
 dp[0][0][0] = 0
 
+# (y, x, k, movements)
+q = deque([(0, 0, 0, 0)])
 while q:
-    y, x, z = q.popleft()
-    movement = dp[y][x][z]
+  y, x, k, movements = q.popleft()
 
-    dxs = [1, -1, 0, 0, 2, 2, 1, 1, -2, -2, -1, -1]
-    dys = [0, 0, 1, -1, 1, -1, 2, -2, 1, -1, 2, -2]
+  if k < K:
+    for dy, dx in KNIGHT:
+      ny, nx = y + dy, x + dx
+      if not (0 <= ny < H and 0 <= nx < W): continue
+      if movements + 1 >= dp[ny][nx][k + 1]: continue
+      if grid[ny][nx] == 1: continue
+      dp[ny][nx][k + 1] = movements + 1
+      q.append((ny, nx, k + 1, movements + 1))
 
-    for i in range(4 if z >= K else 12):
-        new_y, new_x = y + dys[i], x + dxs[i]
-        
-        if not (0 <= new_y < H and 0 <= new_x < W): continue
-        if grid[new_y][new_x] == 1: continue
+  for dy, dx in NORMAL:
+    ny, nx = y + dy, x + dx
+    if not (0 <= ny < H and 0 <= nx < W): continue
+    if movements + 1 >= dp[ny][nx][k]: continue
+    if grid[ny][nx] == 1: continue
+    dp[ny][nx][k] = movements + 1
+    q.append((ny, nx, k, movements + 1))
 
-        # 말의 움직임
-        if i > 3:
-            if -1 < dp[new_y][new_x][z + 1] <= movement + 1: continue
-            dp[new_y][new_x][z + 1] = movement + 1
-            q.append((new_y, new_x, z + 1))
-        # 일반 움직임
-        else:
-            if -1 < dp[new_y][new_x][z] <= movement + 1: continue
-            dp[new_y][new_x][z] = movement + 1
-            q.append((new_y, new_x, z))
-
-ans = -1
-for z in range(K + 1):
-    result = dp[H - 1][W - 1][z]
-    if result != -1 and (ans == -1 or result < ans):
-        ans = result
-
-print(ans)
+ans = min(dp[H - 1][W - 1])
+if ans == float("inf"): print(-1)
+else: print(ans)
